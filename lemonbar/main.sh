f28978b91b7a6aada09d276ea0100fed5c12a8bf
Adam T <ad-t@users.noreply.github.com>
2019-08-25 19:56:36 +1000

    Made xresource selection succeed before generating the bar

--- lemonbar/main.sh
+++ lemonbar/main.sh
@@ -2,6 +2,5 @@
 
 killall lemonbar
 
-COLOR="`$HOME/.config/dotfiles/lemonbar/get_xresource_value.sh color0`"
-
+COLOR="`$HOME/.config/dotfiles/lemonbar/get_xresource_value.sh color0`" && 
 $HOME/.config/dotfiles/lemonbar/feeder.sh | lemonbar -p -d -f "Inconsolata for Powerline-13" -B "$COLOR"
