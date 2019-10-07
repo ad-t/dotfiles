a55e6bd9925974feae0a90d4852589fada9c8d0d
Adam Tizzone <ad-t@users.noreply.github.com>
2019-10-08 01:37:15 +1100

    Added underline to be a consistent color

--- lemonbar/main.sh
+++ lemonbar/main.sh
@@ -2,5 +2,6 @@
 
 killall lemonbar
 
+BLUE_COLOR="`$HOME/.config/dotfiles/lemonbar/get_xresource_value.sh color5`" && 
 COLOR="`$HOME/.config/dotfiles/lemonbar/get_xresource_value.sh color0`" && 
-$HOME/.config/dotfiles/lemonbar/feeder.sh | $HOME/build_repos/lemonbar-xft/lemonbar -p -f "Inconsolata for Powerline-13" -B "$COLOR" -u 3
+$HOME/.config/dotfiles/lemonbar/feeder.sh | $HOME/build_repos/lemonbar-xft/lemonbar -p -f "Inconsolata for Powerline-13" -B "$COLOR" -u 3 -U "$BLUE_COLOR"
