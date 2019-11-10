6812d4c9e73526c383f4cc81f39e0740d575aa85
Adam T <ad-t@users.noreply.github.com>
2019-11-10 19:55:46 +1100

    Changed master font

--- lemonbar/main.sh
+++ lemonbar/main.sh
@@ -4,4 +4,4 @@ killall lemonbar
 
 BLUE_COLOR="`$HOME/.config/dotfiles/lemonbar/get_xresource_value.sh color5`" && 
 COLOR="`$HOME/.config/dotfiles/lemonbar/get_xresource_value.sh color0`" && 
-$HOME/.config/dotfiles/lemonbar/feeder.sh | $HOME/build_repos/lemonbar-xft/lemonbar -p -f "Space Mono for Powerline-12" -B "$COLOR" -u 3 -U "$BLUE_COLOR"
+$HOME/.config/dotfiles/lemonbar/feeder.sh | $HOME/build_repos/lemonbar-xft/lemonbar -p -f "Iosevka Term SS02-12" -B "$COLOR" -u 3 -U "$BLUE_COLOR"
