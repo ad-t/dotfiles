091686f829f1fc3d4eecaa6ec0c8e669346900bd
Adam T <ad-t@users.noreply.github.com>
2020-02-28 16:20:11 +1100

    Changed all fonts across all machines

--- lemonbar/main.sh
+++ lemonbar/main.sh
@@ -10,4 +10,4 @@ $NETMON_PATH/network-monitor.py &
 
 BLUE_COLOR="`$HOME/.config/dotfiles/lemonbar/get_xresource_value.sh color4`" && 
 COLOR="`$HOME/.config/dotfiles/lemonbar/get_xresource_value.sh color0`" && 
-$HOME/.config/dotfiles/lemonbar/feeder.sh | $HOME/build_repos/lemonbar-xft/lemonbar -p -f "Sudo-10" -B "$COLOR" -u 3 -U "$BLUE_COLOR"
+$HOME/.config/dotfiles/lemonbar/feeder.sh | $HOME/build_repos/lemonbar-xft/lemonbar -p -f "Iosevka-10" -B "$COLOR" -u 3 -U "$BLUE_COLOR"
