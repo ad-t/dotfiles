89f5bf70c624d2105ed37ed3f9620bfcce634d73
Adam T <ad-t@users.noreply.github.com>
2020-03-02 15:21:48 +1100

    Changed unifont preference, changed emoji picker to use the complete unicode set

--- emojis/emoji-picker.sh
+++ emojis/emoji-picker.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 EMOJI_FILE="$HOME/.config/dotfiles/emojis/emoji-list.txt"
-EMOJI_FILE_URL="https://unicode.org/Public/emoji/13.0/emoji-sequences.txt"
+EMOJI_FILE_URL="https://unicode.org/Public/emoji/13.0/emoji-test.txt"
 
 # find if one exists
 if [ ! -f "$EMOJI_FILE" ]
@@ -13,14 +13,8 @@ fi
 
 EMOJI_SELECTOR_OUTPUT=$(egrep -v "^[# ]" $EMOJI_FILE \
   | egrep -v "^$" \
-  | cut -d ';' -f 3 \
-  | sed -r "s/^ *//g" \
-  | sed -r "s/ +/ /g" \
-  | sed -r "s/#/-/g" \
-  | sed -r "s/E[0-9]+\.[0-9] +\[[0-9]+\] //g" \
-  | sed -r "s/[()]//g" \
-  | sed -r "s/(.*) - (.*)/\2 - \1/g" \
-  | sed -r "s/\..* -/ -/g" \
+  | sed -r "s/.*# //" \
+  | sed -r "s/E[0-9]+\.[0-9] //g" \
   | rofi -i -dmenu)
 
-echo "$EMOJI_SELECTOR_OUTPUT" | sed "s/ -.*//g" | tr "\n" " " | sed "s/ //g" | xclip -sel CLIPBOARD
+echo "$EMOJI_SELECTOR_OUTPUT" | sed "s/ .*//g" | tr "\n" " " | sed "s/ //g" | xclip -sel CLIPBOARD
