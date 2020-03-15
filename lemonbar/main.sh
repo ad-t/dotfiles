d41d5264e9c65670f2079cf8151540b6ceb385e7
Adam T <ad-t@users.noreply.github.com>
2020-03-15 23:00:24 +1100

    Font changes

--- lemonbar/main.sh
+++ lemonbar/main.sh
@@ -10,4 +10,4 @@ $NETMON_PATH/network-monitor.py &
 
 BLUE_COLOR="`$HOME/.config/dotfiles/lemonbar/get_xresource_value.sh color4`" && 
 COLOR="`$HOME/.config/dotfiles/lemonbar/get_xresource_value.sh color0`" && 
-$HOME/.config/dotfiles/lemonbar/feeder.sh | $HOME/build_repos/lemonbar-xft/lemonbar -p -f "Monaco-8" -B "$COLOR" -u 3 -U "$BLUE_COLOR"
+$HOME/.config/dotfiles/lemonbar/feeder.sh | $HOME/build_repos/lemonbar-xft/lemonbar -p -f "DejaVu Sans Mono-10" -B "$COLOR" -u 3 -U "$BLUE_COLOR"
