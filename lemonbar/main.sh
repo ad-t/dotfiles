28f0ceb6df0ce4b48e20074893aba82de6894795
Adam T <ad-t@users.noreply.github.com>
2020-09-13 20:19:04 +1000

    Update font to purely sans font

--- lemonbar/main.sh
+++ lemonbar/main.sh
@@ -11,4 +11,4 @@ $NETMON_PATH/network-monitor.py &
 ACCENT_COLOR="`$HOME/.config/dotfiles/lemonbar/get_xresource_value.sh color7`" && 
 COLOR="`$HOME/.config/dotfiles/lemonbar/get_xresource_value.sh color0`" && 
 # $HOME/.config/dotfiles/lemonbar/feeder.sh | $HOME/build_repos/lemonbar-xft/lemonbar -p -f "FiraCode-10" -B "$COLOR" -u 3 -U "$ACCENT_COLOR"
-conky | $HOME/build_repos/lemonbar-xft/lemonbar -p -f "FiraCode-10" -B "$COLOR" -u 3 -U "$ACCENT_COLOR"
+conky | $HOME/build_repos/lemonbar-xft/lemonbar -p -f "Deja Vu Sans Mono-10" -B "$COLOR" -u 3 -U "$ACCENT_COLOR"
