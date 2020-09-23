73e75f714a2950481ef97c7e118ec26ff5d78e45
Adam T <ad-t@users.noreply.github.com>
2020-09-23 23:48:40 +1000

    Update fonts to an updated version of Iosevka Fixed Extended

--- lemonbar/main.sh
+++ lemonbar/main.sh
@@ -11,4 +11,4 @@ $NETMON_PATH/network-monitor.py &
 ACCENT_COLOR="`$HOME/.config/dotfiles/lemonbar/get_xresource_value.sh color7`" && 
 COLOR="`$HOME/.config/dotfiles/lemonbar/get_xresource_value.sh color0`" && 
 # $HOME/.config/dotfiles/lemonbar/feeder.sh | $HOME/build_repos/lemonbar-xft/lemonbar -p -f "FiraCode-10" -B "$COLOR" -u 3 -U "$ACCENT_COLOR"
-conky | $HOME/build_repos/lemonbar-xft/lemonbar -p -f "Iosevka Term SS02:Bold:size=10" -B "$COLOR" -u 3 -U "$ACCENT_COLOR"
+conky | $HOME/build_repos/lemonbar-xft/lemonbar -p -f "Iosevka Fixed Extended:Bold:size=10" -B "$COLOR" -u 3 -U "$ACCENT_COLOR"
