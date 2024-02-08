52b49bf33afcb09c6866a5e60cd5f87cbf3a1d45
Adam T <ad-t@users.noreply.github.com>
2024-02-08 23:24:44 +1100

    Using nvchad

---
+++ nvim/lua/custom/init.lua
@@ -0,0 +1,14 @@
+-- local autocmd = vim.api.nvim_create_autocmd
+
+-- Auto resize panes when resizing nvim window
+-- autocmd("VimResized", {
+--   pattern = "*",
+--   command = "tabdo wincmd =",
+-- })
+--
+-- Set cursor to block
+vim.opt.guicursor = ""
+
+-- Remap jk and kj to be escape characters
+vim.keymap.set('i', 'kj', "<Esc>")
+vim.keymap.set('i', 'jk', "<Esc>")
