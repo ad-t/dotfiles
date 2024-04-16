f6a39dc12bbdce05289915afe433552926155147
Adam T <ad-t@users.noreply.github.com>
2024-04-16 10:39:54 +1000

    Fish theme, but a little fancier

--- fish/theme.fish
+++ fish/theme.fish
@@ -14,14 +14,17 @@ end
 
 function git_segment 
   set GIT "$argv[1]"
+  separator "blue" "yellow"
   set_color black
   set_color -b yellow
   if [ "$GIT" != "" ]
     segment (echo $GIT | string replace '(' ' ' | string replace ')' ' ' | string trim --left --right)
   end
+  separator "yellow" "black"
 end
 
 function right_prompt
+  separator black yellow
   set_color black
   set_color -b yellow
   segment (date +%T)
@@ -33,5 +36,17 @@ function status_segment
     set_color black
     set_color -b red
     segment "$STATUS"
+    separator red blue
   end
 end
+
+function separator
+  set BEFORE "$argv[1]"
+  set AFTER "$argv[2]"
+
+  set_color "$BEFORE"
+  set_color -b "$AFTER"
+  printf "\ue0bc"
+end
+
+# powerline symbols: https://github.com/ryanoasis/powerline-extra-symbols
