e651cf2b1eedda7cfcb0f75754442feb03606704
Adam T <ad-t@users.noreply.github.com>
2024-04-30 21:02:26 +1000

    Fixing LSPs and treesitter languages

--- nvim/lua/plugins/init.lua
+++ nvim/lua/plugins/init.lua
@@ -8,31 +8,56 @@ return {
   },
 
   -- These are some examples, uncomment them if you want to see them work!
-  -- {
-  --   "neovim/nvim-lspconfig",
-  --   config = function()
-  --     require("nvchad.configs.lspconfig").defaults()
-  --     require "configs.lspconfig"
-  --   end,
-  -- },
-  --
-  -- {
-  -- 	"williamboman/mason.nvim",
-  -- 	opts = {
-  -- 		ensure_installed = {
-  -- 			"lua-language-server", "stylua",
-  -- 			"html-lsp", "css-lsp" , "prettier"
-  -- 		},
-  -- 	},
-  -- },
-  --
-  -- {
-  -- 	"nvim-treesitter/nvim-treesitter",
-  -- 	opts = {
-  -- 		ensure_installed = {
-  -- 			"vim", "lua", "vimdoc",
-  --      "html", "css"
-  -- 		},
-  -- 	},
-  -- },
+  {
+    "neovim/nvim-lspconfig",
+    config = function()
+      require("nvchad.configs.lspconfig").defaults() require "configs.lspconfig"
+    end,
+  },
+
+  {
+    "williamboman/mason.nvim",
+    opts = {
+      ensure_installed = {
+        "lua-language-server",
+        "stylua",
+        "html-lsp",
+        "css-lsp" ,
+        "prettier",
+        "bash-language-server",
+        "css-lsp",
+        "docker-compose-language-service",
+        "dockerfile-language-server",
+        "dprint",
+        "eslint-lsp",
+        "html-lsp",
+        "json-lsp",
+        "python-lsp-server",
+        "tailwindcss-language-server",
+        "terraform-ls",
+        "tflint",
+        "typescript-language-server",
+        "yaml-language-server"
+      },
+    },
+  },
+
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = {
+      ensure_installed = {
+        "vim",
+        "lua",
+        "vimdoc",
+        "html",
+        "css",
+        "typescript",
+        "javascript",
+        "python",
+        "yaml",
+        "dockerfile",
+        "terraform"
+      },
+    },
+  },
 }
