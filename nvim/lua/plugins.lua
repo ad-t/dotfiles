b0bcb8155d06ae89ace15266cfd2ae174d177bb4
Adam T <ad-t@users.noreply.github.com>
2024-09-22 14:12:02 +1000

    Fixing fzf

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -113,11 +113,13 @@ return {
   -- file searching -- super fast!
   {
     "ibhagwan/fzf-lua",
+    lazy = true,
     keys = {
       { "<leader>ff", "<cmd>lua require('fzf-lua').files()<cr>", { silent = true } },
     },
     dependencies = { "nvim-tree/nvim-web-devicons" },
     opts = {
+      fzf_args = vim.env.FZF_DEFAULT_OPTS,
       winopts = {
         border = false,
         preview = {
